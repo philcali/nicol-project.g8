cdd7f6f19114581bacd72aceb7d8d06ea497038f
Philip Cali <philip.cali@gmail.com>
2011-06-06 14:38:09 -0500

    People may want to override the Nicol version.

--- src/main/g8/project/build/project.scala
+++ src/main/g8/project/build/project.scala
@@ -1,5 +1,7 @@
 import sbt._
 
 class Project(info: ProjectInfo) extends LWJGLProject(info) with Nicol {
+  override def nicolVersion = "$nicol_version$"
+
   override def mainClass = Some("$package$.$game_class$")
 }
